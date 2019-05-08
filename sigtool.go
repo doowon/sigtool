297453746f4c7a1b3f5a83482c04091bd8e336b2
Doowon Kim <gotapenny@gmail.com>
2019-05-08 11:08:10 -0400

    added extract digital signature

---
+++ sigtool.go
@@ -0,0 +1,39 @@
+package sigtool
+
+import (
+	"debug/pe"
+	"log"
+	"os"
+)
+
+// ExtractDigitalSignature extracts a digital signature specified in a signed PE file.
+// It returns a digital signature (pkcs#7) in bytes.
+func ExtractDigitalSignature(filePath string) (buf []byte) {
+	pefile, err := pe.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pefile.Close()
+
+	var vAddr uint32
+	var size uint32
+	switch t := pefile.OptionalHeader.(type) {
+	case *pe.OptionalHeader32:
+		vAddr = t.DataDirectory[pe.IMAGE_DIRECTORY_ENTRY_SECURITY].VirtualAddress
+		size = t.DataDirectory[pe.IMAGE_DIRECTORY_ENTRY_SECURITY].Size
+	case *pe.OptionalHeader64:
+		vAddr = t.DataDirectory[pe.IMAGE_DIRECTORY_ENTRY_SECURITY].VirtualAddress
+		size = t.DataDirectory[pe.IMAGE_DIRECTORY_ENTRY_SECURITY].Size
+	}
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	buf = make([]byte, int64(size))
+	f.ReadAt(buf, int64(vAddr+8))
+
+	return buf
+}
